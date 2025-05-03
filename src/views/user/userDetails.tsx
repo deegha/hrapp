6c61b52d71964d9787ecdc4b6e61936f1a70a6da
deegha <shuboothi@gmail.com>
2025-05-03 15:52:32 +0800

    Enhanced user experiance on the user details

--- src/views/user/userDetails.tsx
+++ src/views/user/userDetails.tsx
@@ -1,5 +1,6 @@
 import { TAllUserDetails } from "@/types/user";
 import { StatusTag, Button } from "@/components";
+import { Trash, CheckCircle, EyeOff, Edit } from "react-feather";
 
 interface IUserDetails {
   user: TAllUserDetails;
@@ -44,7 +45,7 @@ export function UserDetails({ user }: IUserDetails) {
         <h1 className="text-[32px] font-semibold uppercase">
           {user.firstName} {user.lastName} (EMP-{user.employeeId})
         </h1>
-        <StatusTag type="ACTIVE" status={user.UserStatus?.statusLabel} />
+        <StatusTag status={user.UserStatus?.statusLabel} />
       </div>
       <div className="flex flex-col gap-3">
         <h2>User Details</h2>
@@ -56,7 +57,7 @@ export function UserDetails({ user }: IUserDetails) {
       <div className="flex flex-col gap-3">
         <h2>Most Recent User Logs</h2>
         {logs.map((log, i) => (
-          <div key={i} className="flex flex-col gap-1">
+          <div key={i} className="flex flex-col gap-1 ">
             <div className="flex text-textSecondary font-semibold">
               <div className="w-2 h-2 bg-primary rounded-full" />
               <div className="text-sm ">{log.log}</div>
@@ -68,9 +69,25 @@ export function UserDetails({ user }: IUserDetails) {
         ))}
       </div>
       <div className="flex gap-3  w-full">
-        <Button>Edit</Button>
-        <Button variant="secondary">Deactivate</Button>
-        <Button variant="danger">Button</Button>
+        {user.UserStatus?.statusLabel === "CREATED" && (
+          <Button>
+            <div className="flex gap-1 items-center">
+              <CheckCircle size={14} /> Approve
+            </div>
+          </Button>
+        )}
+        <Button variant="secondary">
+          <div className="flex gap-1 items-center">
+            <Edit size={14} /> Edit
+          </div>
+        </Button>
+
+        <Button variant="danger">
+          <div className="flex gap-1 items-center">
+            <Trash size={14} />
+            Delete
+          </div>
+        </Button>
       </div>
     </div>
   );
